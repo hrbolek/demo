3586cd7e8f0a5690ac2a1115c5fe12d3d3761f8a
Hrbolek <alexandr.stefek@unob.cz>
2023-04-12 22:00:04 +0200

    Lesson 13th April

--- src/queries/GroupQuerySmall.js
+++ src/queries/GroupQuerySmall.js
@@ -1,27 +1,24 @@
 import { authorizedFetch } from 'queries/authorizedFetch'
 
+
+export const GroupQuerySmallJSON = (id) => ({
+    "query":
+        `query ($id: ID!) {
+            groupById(id: $id) {
+                id, name, valid, lastchange
+                grouptype { id, name }
+                roles {
+                    id, user { id, name,surname, email }
+                }
+                subgroups {
+                    id, name, valid, lastchange, grouptype { id, name }
+                }
+            }
+        }`,
+    "variables": {"id": id}
+})
+
 export const GroupQuerySmall = (id) =>
     authorizedFetch('/gql', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        redirect: 'follow', // manual, *follow, error
-        body: JSON.stringify({
-            "query":
-                `query ($id: ID!) {
-                    groupById(id: $id) {
-                        id, name, valid, lastchange
-                        grouptype { id, name }
-                        roles {
-                            id, user { id, name,surname, email }
-                        }
-                        subgroups {
-                            id, name, valid, lastchange, grouptype { id, name }
-                        }
-                    }
-                }`,
-            "variables": {"id": id}
-        }),
+        body: JSON.stringify(GroupQuerySmallJSON(id)),
     })
